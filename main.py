67541792b875c6b76f762292c201b481b65b2beb
Gregory Duke <32493649+gregorywduke@users.noreply.github.com>
2023-05-28 16:01:29 -0500

    Updated GUI layout, added count feature

--- main.py
+++ main.py
@@ -5,12 +5,16 @@ sg.theme('DarkAmber')
 
 # GUI layout
 layout = [[sg.Text("Select iNaturalist export (.csv)")],
-          [sg.FileBrowse("Select File", key="csv"), sg.Text("",key="fb" )],
+          [sg.FileBrowse("Select File", key="csv"), sg.Text("", key="fb" )],
           [sg.Text("Select folder to store downloaded files")],
-          [sg.FolderBrowse("Select Folder", key="dir"), sg.Text("",key="folb" )],
+          [sg.FolderBrowse("Select Folder", key="dir"), sg.Text("", key="folb" )],
+          [sg.Text("")],
+          [sg.Text("Enter last file number:", key="filenum"), sg.InputText(key="NUM"),
+           sg.Button("Enter")],
           [sg.Button("Start Download"), sg.Text("Idle...", key="out")]]
 
 window = sg.Window("iNaturalistScraper", layout)
+count = 0
 
 # Event Loop
 while True:
@@ -21,9 +25,14 @@ while True:
     if event == "Select File":
         # Updates value to display file selected by user
         window["fb"].update(values["csv"])
+        print("Folder selected: ", values["csv"])
     if event == "Select Folder":
         # Updates value to display folder selected by user
+        print("Folder selected: ", values["dir"])
         window["folb"].update(values["dir"])
+    if event == "Enter":
+        count = values["NUM"]
+        print("File number continues after ", values["NUM"])
     if event == "Start Download":
         source = values["csv"]
         dir = values["dir"]
@@ -31,7 +40,6 @@ while True:
 
         # Open .CSV file
         file = open(source)
-        count = 0
 
         # Read and download each URL in .CSV
         for x in file:
@@ -40,5 +48,6 @@ while True:
             urllib.request.urlretrieve(x, fname)
 
         window["out"].update("Downloaded.")
+        print("Downloaded files.")
         file.close()
 
